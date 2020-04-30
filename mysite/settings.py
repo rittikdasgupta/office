e9bdc7ad240253a0ac1460f6269c7c1442debde9
AryanDutta <aryandutta715@gmail.com>
2020-04-30 20:16:30 +0530

    Templates and blogs

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'office',
+        'USER': 'postgres',
+        'PASSWORD': '782460',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
