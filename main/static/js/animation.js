4c1c612038d84babe6441545b24c25b5b06618d1
rittick-dasgupta1234 <hrkdasgupta025@gmail.com>
2020-05-19 20:28:05 +0530

    Updated Main

--- main/static/js/animation.js
+++ main/static/js/animation.js
@@ -1,3 +1,4 @@
+//LARGE SCREEN VIEW ANIMATION OF PROMISE SECTION
 const active = () => {
     const btn=document.querySelector(".a1");
 
@@ -40,3 +41,41 @@ const active3 = () => {
 active();
 active2();
 active3();
+//END LARGE SCREEN VIEW
+
+//MOBILE VIEW NAV SLIDE IN
+const slider = () => {
+    const hamburger=document.querySelector('.hamburger');
+    const navSlide=document.querySelector('.slider-main');
+    hamburger.addEventListener('click', () => {
+        navSlide.classList.toggle('active');
+        scrollDisable();
+    });
+}
+const sliderOut = () =>{
+    const arrow=document.querySelector('.arrow');
+    const navSlide=document.querySelector('.slider-main');
+    arrow.addEventListener('click', () => {
+        navSlide.classList.toggle('active');
+        scrollActive();
+    });
+}
+function scrollDisable(){
+    document.querySelector('html').style.position='fixed';
+}
+function scrollActive(){
+    document.querySelector('html').style.position='static';
+}
+function sliderOutOnBodyTouch(){
+    const doc=document.querySelector('#Opac-slider');
+    const navSlide=document.querySelector('.slider-main');
+    doc.addEventListener('touchend', () => {
+        if(navSlide.classList.contains('active')){
+            navSlide.classList.remove('active');
+        }
+    });
+}
+slider();
+sliderOut();
+sliderOutOnBodyTouch();
+//END MOBILE VIEW NAV SLIDE IN
