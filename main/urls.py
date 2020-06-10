6a1a1d8ee81287d127cad72d3741580b622621a4
rittick-dasgupta1234 <hrkdasgupta025@gmail.com>
2020-06-10 21:03:53 +0530

    GD Mobile Optimised and JS Bug Fixed

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("main/", views.mainpage, name="main page"),
     path("contact/", views.contact, name="contact"),
     path("wd/", views.webdesign, name="webdesign"),
     path("graphics/", views.graphicsdesign, name="graphicsdesign"),
--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request,'main/index.html')
+def mainpage(request):
+    return render(request,'main/main.html')
 
 def contact(request):
     return render(request, 'main/contact.html')
